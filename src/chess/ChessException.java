87a2bb3e622155081281e436f8215a7273f92602
Talisson Bruno <talissonbrsouza@gmail.com>
2019-03-16 00:41:09 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class ChessException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChessException(String mgs) {
+		super(mgs);
+	}
+}
