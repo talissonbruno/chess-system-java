29e1480a3eaf7473772be1d4004b994463516ca8
Talisson Bruno <talissonbrsouza@gmail.com>
2019-05-03 10:42:06 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,11 +21,11 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-			
+
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-			
+
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			}
 			catch (ChessException e) {
